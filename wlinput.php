edaddf76552363ef64d459d46516e71d500262e2
Thomas <mike2k@mail.ru>
2016-06-25 06:52:43 +0600

    ва

--- wlinput.php
+++ wlinput.php
@@ -1,4 +1,12 @@
 <?php
+switch ($_SERVER['HTTP_ORIGIN']) {
+    case 'https://thomaswl.github.io': case 'https://thomaswl.github.io':
+    header('Access-Control-Allow-Origin: '.$_SERVER['HTTP_ORIGIN']);
+    header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+    header('Access-Control-Max-Age: 1000');
+    header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+    break;
+}
 if (isset($_POST['wlinp'])) {$wlinp = $_POST['wlinp']; }
 if (isset($_POST['wlname'])) {$wlname = $_POST['wlname']; }
 
